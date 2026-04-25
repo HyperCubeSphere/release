4c240b051d971624a733b2d99509a89d4fd755c3
TED Vortex <ted.vortex@gmail.com>
2026-04-26 01:17:00 +0200

    feat: initial commit, create node deno and python release management

---
+++ README.md
@@ -0,0 +1,235 @@
+<div align="center">
+  <img src="https://gist.githubusercontent.com/0-vortex/3acb60674856c437c86bee683d96515b/raw/fab74e1934e5a8686a3190f4d53606bb11da4d96/logo.png" width="400">
+
+# @hypercubesphere/release
+
+> [**semantic-release**](https://github.com/semantic-release/semantic-release) shareable config to publish to `npm` and/or `ghcr`.
+
+> now available as a [GitHub Marketplace action](https://github.com/marketplace/actions/hypercubesphere-release)
+
+> works with node, deno and python
+
+[![Commits](https://img.shields.io/github/commit-activity/w/HyperCubeSphere/release?style=flat)](https://github.com/HyperCubeSphere/release/pulse)
+[![Issues](https://img.shields.io/github/issues/HyperCubeSphere/release.svg?style=flat)](https://github.com/HyperCubeSphere/release/issues)
+[![Releases](https://img.shields.io/github/v/release/HyperCubeSphere/release.svg?style=flat)](https://github.com/HyperCubeSphere/release/releases)
+
+</div>
+
+## 📦 Plugins
+
+This shareable configuration use the following plugins:
+
+- [`@semantic-release/commit-analyzer`](https://github.com/semantic-release/commit-analyzer)
+- [`@semantic-release/release-notes-generator`](https://github.com/semantic-release/release-notes-generator)
+- [`@semantic-release/changelog`](https://github.com/semantic-release/changelog)
+- [`conventional-changelog-conventionalcommits`](https://github.com/conventional-changelog/conventional-changelog)
+- [`@semantic-release/npm`](https://github.com/semantic-release/npm)
+- [`semantic-release-replace-plugin`](https://github.com/jpoehnelt/semantic-release-replace-plugin)
+- [`@semantic-release/git`](https://github.com/semantic-release/git)
+- [`@semantic-release/github`](https://github.com/semantic-release/github)
+- [`@eclass/semantic-release-docker`](https://github.com/eclass/semantic-release-docker)
+- [`@semantic-release/exec`](https://github.com/semantic-release/exec)
+- [`semantic-release-major-tag`](https://github.com/doteric/semantic-release-major-tag)
+- [`npmlog`](https://github.com/npm/npmlog)
+
+## 🖥️ Requirements
+
+Most important limitations are:
+- `GITHUB_TOKEN` for everything
+- `NPM_TOKEN` for public `npm` library
+- `docker` containers need to be built beforehand
+
+You can skip here if you are using elevated [Private Access Token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token), however we don't recommend going down that path.
+
+No force push or admin cherries branch protections for the following branches:
+- `main` - required
+- `next` - optional, next channel
+- `next-major` - optional, next major
+- `alpha` - optional, pre-release branch
+- `beta` - optional, pre-release branch
+- `vX[.X.X]` - maintenance releases
+
+If you use more than the main branch, optionally create an environment that is limiting where pushes can come from and enable the merge strategy.
+
+We are using `production` in our examples, if you copy paste them you will find this new environment generated in your settings! 🍕
+
+## 🧪 GitHub actions usage
+
+Since version 3 it is possible to use semantic-release without any trace of it or the open-sauced configuration anywhere in the dependency tree.
+
+Docker containers are pushed as part of the release so they mirror the availability of `npm` packages.
+
+The simplest use case for a typical NPM package, almost zero install downtime from ghcr and no more local tooling:
+
+```yaml
+name: "Release container"
+
+on:
+  push:
+    branches:
+      - main
+      - next
+      - next-major
+      - alpha
+      - beta
+
+jobs:
+  release:
+    environment:
+      name: production
+      url: https://github.com/${{ github.repository }}/releases/tag/${{ env.RELEASE_TAG }}
+    runs-on: ubuntu-latest
+    steps:
+      - name: "☁️ checkout repository"
+        uses: actions/checkout@v4
+        with:
+          fetch-depth: 0
+
+      - name: "🚀 release"
+        id: semantic-release
+        uses: docker://ghcr.io/hypercubesphere/release:1.0.0
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          NPM_TOKEN: ${{ secrets.NPM_TOKEN }}
+
+      - name: '♻️ cleanup'
+        run: |
+          echo ${{ env.RELEASE_TAG }}
+          echo ${{ env.RELEASE_VERSION }}
+```
+
+Marketplace actions should default to the major tag and are essentially more stable as we have to curate every release.
+
+A more traditional approach, only thing really different here is a minor pull overhead and using set outputs instead of environment variables:
+
+```yaml
+name: "Release"
+
+on:
+  push:
+    branches:
+      - main
+      - next
+      - next-major
+      - alpha
+      - beta
+
+jobs:
+  release:
+    environment:
+      name: production
+      url: https://github.com/${{ github.repository }}/releases/tag/${{ steps.semantic-release.outputs.release-tag }}
+    name: Semantic release
+    runs-on: ubuntu-latest
+    steps:
+      - name: "☁️ checkout repository"
+        uses: actions/checkout@v4
+        with:
+          fetch-depth: 0
+
+      - name: "🚀 release"
+        id: semantic-release
+        uses: HyperCubeSphere/release@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          NPM_TOKEN: ${{ secrets.NPM_TOKEN }}
+
+      - name: '♻️ cleanup'
+        run: |
+          echo ${{ steps.semantic-release.outputs.release-tag }}
+          echo ${{ steps.semantic-release.outputs.release-version }}
+```
+
+## 📦 NPM usage
+
+You can opt to use this package in your local tooling. Proceed as you would normally would, replacing `npm` with your package manager of choice and install the package:
+
+```bash
+npm install --save-dev release
+```
+
+The shareable config can then be configured in the [**semantic-release** configuration file](https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration):
+
+```json
+{
+  "extends": "release"
+}
+```
+
+Now all you need to do is create a release:
+
+```shell
+npx semantic-release
+```
+
+## 🔧 Configuration
+
+See each [plugin](#-plugins) documentation for required installation and configuration steps.
+
+### NPM
+
+Set `private` to true in `package.json` if you want to disable `npm`, or, change the scope of package using `publishConfig`.
+
+Keep one of `files` or `main` keys in your `package.json` accurate depending on whether you are building a library or an application.
+
+If you publish, make sure to also provide a valid `NPM_TOKEN` as `.npmrc` authentication is ignored in our config!
+
+### GitHub Actions
+
+Unless you have an `action.yml` present in your root folder, this module is not added to the release config.
+
+If you have an `action.yml` present, our config will attempt to adjust the container version to the newly pushed `npm` and `docker` tags.
+
+### Docker
+
+Unless you have a `Dockerfile` present in your root folder, this module is not added to the release config.
+
+If you have a `Dockerfile` present, our config will attempt to push to `ghcr.io`.
+
+### Environment variables
+
+Using our configuration comes with some sensible defaults:
+
+- `DOCKER_USERNAME=$GITHUB_REPOSITORY_OWNER`
+- `DOCKER_PASSWORD=$GITHUB_TOKEN`
+- `GIT_COMMITTER_NAME="hypercubesphere[bot]"`
+- `GIT_COMMITTER_EMAIL="279296405+hypercubesphere[bot]@users.noreply.github.com"`
+- `GIT_AUTHOR_NAME` - parsed from commit `$GITHUB_SHA`
+- `GIT_AUTHOR_EMAIL` - parsed from commit `$GITHUB_SHA`
+
+Feel free to change any of the above to whatever suits your purpose, our motivation is to keep `GITHUB_TOKEN` and/or `NPM_TOKEN` the only necessary requirements.
+
+We are actively investigating ways to drop the 2 remaining variables as well!
+
+## 🚧 Disabling auto-detect features
+
+You can use these environment variables to deny execution of any part of the auto-detect features:
+
+- `SR_DISABLE_CHANGELOG`
+- `SR_DISABLE_NPM`
+- `SR_DISABLE_DENO`
+- `SR_DISABLE_ACTIONS`
+- `SR_DISABLE_DOCKER`
+
+## 🤝 Contributing
+
+If you decide to fix a bug, make sure to use the conventional commit available at:
+
+```shell
+npm run push
+```
+
+### Disabling auto-detect features
+
+You can use these environment variables to deny execution of any part of the auto-detect features:
+
+- `SR_DISABLE_CHANGELOG`
+- `SR_DISABLE_NPM`
+- `SR_DISABLE_ACTIONS`
+- `SR_DISABLE_CHROME`
+- `SR_DISABLE_DOCKER`
+- `SR_DISABLE_PYTHON`
+
+## ⚖️ LICENSE
+
+MIT © [TED (Teodor-Eugen Dutulescu) Vortex](./LICENSE)
---
+++ release.config.js
@@ -0,0 +1,318 @@
+const { existsSync } = require("node:fs");
+const { execSync } = require('node:child_process');
+const log = require("npmlog");
+
+log.info(`Executing semantic-release config setup`);
+
+const releaseConfig = {
+  "branches": [
+    // maintenance releases
+    "+([0-9])?(.{+([0-9]),x}).x",
+
+    // release channels
+    "main",
+    "next",
+    "next-major",
+
+    // pre-releases
+    {
+      name: "beta",
+      prerelease: true
+    },
+    {
+      name: "alpha",
+      prerelease: true
+    }
+  ],
+  plugins: [],
+}
+const noteKeywords = [
+  "BREAKING CHANGE",
+  "BREAKING CHANGES",
+  "BREAKING"
+];
+const {
+  GITHUB_SHA,
+  GITHUB_ACTIONS,
+  GITHUB_REPOSITORY,
+  GIT_COMMITTER_NAME,
+  GIT_COMMITTER_EMAIL,
+  GIT_AUTHOR_NAME,
+  GIT_AUTHOR_EMAIL,
+  SR_DISABLE_CHANGELOG,
+  SR_DISABLE_NPM,
+  SR_DISABLE_DENO,
+  SR_DISABLE_ACTIONS,
+  SR_DISABLE_CHROME,
+  SR_DISABLE_DOCKER,
+  SR_DISABLE_PYTHON,
+} = process.env;
+const [owner, repo] = String(GITHUB_REPOSITORY).toLowerCase().split("/");
+const addPlugin = (plugin, options) => {
+  log.info(`${plugin} enabled with${options && ' options:' || 'out options'}`);
+  options && log.info(null, options);
+  return releaseConfig.plugins.push([plugin, options]);
+};
+
+!GIT_COMMITTER_NAME && (process.env.GIT_COMMITTER_NAME = "hypercubesphere[bot]");
+!GIT_COMMITTER_EMAIL && (process.env.GIT_COMMITTER_EMAIL = "279296405+hypercubesphere[bot]@users.noreply.github.com");
+
+try {
+  const authorName = execSync(`git log -1 --pretty=format:%an ${GITHUB_SHA}`, { encoding: "utf8", stdio: "pipe" });
+  const authorEmail = execSync(`git log -1 --pretty=format:%ae ${GITHUB_SHA}`, { encoding: "utf8", stdio: "pipe" });
+  authorName && !GIT_AUTHOR_NAME && (process.env.GIT_AUTHOR_NAME = `${authorName}`);
+  authorEmail && !GIT_AUTHOR_EMAIL && (process.env.GIT_AUTHOR_EMAIL = `${authorEmail}`);
+} catch (e) {
+  log.warn(`Unable to set GIT_COMMITTER_NAME and GIT_COMMITTER_EMAIL`);
+  log.error(e);
+}
+
+log.info(`Adding semantic-release config plugins`);
+
+addPlugin("@semantic-release/commit-analyzer", {
+  "preset": "conventionalcommits",
+  "releaseRules": [
+    {breaking: true, release: "major"},
+    {type: "feat", release: "minor"},
+    {type: "fix", release: "patch"},
+    {type: "perf", release: "patch"},
+    {type: "revert", release: "patch"},
+    {type: "docs", release: "minor"},
+    {type: "style", release: "patch"},
+    {type: "refactor", release: "patch"},
+    {type: "test", release: "patch"},
+    {type: "build", release: "patch"},
+    {type: "ci", release: "patch"},
+    {type: "chore", release: false}
+  ],
+  "parserOpts": {
+    noteKeywords
+  }
+});
+
+addPlugin("@semantic-release/release-notes-generator", {
+  "preset": "conventionalcommits",
+  "parserOpts": {
+    noteKeywords
+  },
+  "writerOpts": {
+    "commitsSort": ["subject", "scope"]
+  },
+  "presetConfig": {
+    types: [
+      {type: "feat", section: "🍕 Features"},
+      {type: "feature", section: "🍕 Features"},
+      {type: "fix", section: "🐛 Bug Fixes"},
+      {type: "perf", section: "🔥 Performance Improvements"},
+      {type: "revert", section: "⏩ Reverts"},
+      {type: "docs", section: "📝 Documentation"},
+      {type: "style", section: "🎨 Styles"},
+      {type: "refactor", section: "🧑‍💻 Code Refactoring"},
+      {type: "test", section: "✅ Tests"},
+      {type: "build", section: "🤖 Build System"},
+      {type: "ci", section: "🔁 Continuous Integration"}
+    ]
+  }
+});
+
+SR_DISABLE_CHANGELOG === undefined && addPlugin("@semantic-release/changelog", {
+  "changelogTitle": `# 📦 ${owner}/${repo} changelog
+
+[![conventional commits](https://img.shields.io/badge/conventional%20commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
+[![semantic versioning](https://img.shields.io/badge/semantic%20versioning-2.0.0-green.svg)](https://semver.org)
+
+> All notable changes to this project will be documented in this file`
+});
+
+const pkgExists = existsSync("./package.json");
+if (pkgExists && SR_DISABLE_NPM === undefined) {
+  addPlugin("@semantic-release/npm", {
+    "tarballDir": "pack"
+  });
+}
+
+const denoExists = existsSync("./deno.json");
+if (denoExists && SR_DISABLE_DENO === undefined) {
+  addPlugin("semantic-release-replace-plugin", {
+    "replacements": [{
+      "files": [
+        "deno.json"
+      ],
+      "from": `"version":.*".*"`,
+      "to": `"version": "\${nextRelease.version}"`,
+      "results": [{
+        "file": "deno.json",
+        "hasChanged": true,
+        "numMatches": 1,
+        "numReplacements": 1
+      }],
+      "countMatches": true
+    }]
+  });
+}
+
+const pythonExists = existsSync("./pyproject.toml");
+if (pythonExists && SR_DISABLE_PYTHON === undefined) {
+  addPlugin("semantic-release-replace-plugin", {
+    "replacements": [{
+      "files": [
+        "pyproject.toml"
+      ],
+      "from": `^version.*=.*".*"$`,
+      "to": `version = "\${nextRelease.version}"`,
+      "results": [{
+        "file": "pyproject.toml",
+        "hasChanged": true,
+        "numMatches": 1,
+        "numReplacements": 1
+      }],
+      "countMatches": true
+    }]
+  });
+
+  const uvLockExists = existsSync("./uv.lock");
+  if (uvLockExists) {
+    let pyProjectName;
+    try {
+      const pyproject = execSync(`cat pyproject.toml`, { encoding: "utf8", stdio: "pipe" });
+      const projectTable = pyproject.match(/\[project\]([\s\S]*?)(?=\n\[|$)/);
+      const nameMatch = projectTable && projectTable[1].match(/\n\s*name\s*=\s*"([^"]+)"/);
+      pyProjectName = nameMatch && nameMatch[1];
+    } catch (e) {
+      log.warn(`Unable to read package name from pyproject.toml`);
+      log.error(e);
+    }
+
+    if (!pyProjectName) {
+      log.warn(`No package name found in pyproject.toml [project] table, skipping uv.lock update`);
+    } else {
+      log.info(`uv.lock update enabled for package "${pyProjectName}"`);
+      const escapedName = pyProjectName.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      addPlugin("semantic-release-replace-plugin", {
+        "replacements": [{
+          "files": [
+            "uv.lock"
+          ],
+          "from": `(\\[\\[package\\]\\]\\nname = "${escapedName}"\\nversion = ")[^"]*(")`,
+          "to": `$1<%= nextRelease.version.replace(/-(alpha|beta|rc)\\.?(\\d*)/i, function(m, p, n) { return ({alpha:"a", beta:"b", rc:"rc"})[p.toLowerCase()] + (n || "0"); }) %>$2`,
+          "results": [{
+            "file": "uv.lock",
+            "hasChanged": true,
+            "numMatches": 1,
+            "numReplacements": 1
+          }],
+          "countMatches": true
+        }]
+      });
+    }
+  }
+}
+
+const actionExists = existsSync("./action.yml");
+if (actionExists && SR_DISABLE_ACTIONS === undefined) {
+  // regex the content of action.yml to replace the image tag
+  const actionYml = execSync(`cat action.yml`, { encoding: "utf8", stdio: "pipe" });
+  const re = new RegExp(`image:\\s'docker:\/\/ghcr.io\/${owner}\/${repo}:.*'`, "g");
+  const imageTag = actionYml.match(re);
+
+  if (!imageTag) {
+    log.warn(`Unable to find image tag in action.yml, no action will be published to the GitHub Container Registry`);
+    log.warn(`Please add the following to your action.yml file:`);
+    log.warn(`image: 'docker://ghcr.io/${owner}/${repo}:latest'`);
+  }
+
+  imageTag && addPlugin("semantic-release-replace-plugin", {
+    "replacements": [{
+      "files": [
+        "action.yml"
+      ],
+      "from": `image: 'docker://ghcr.io/${owner}/${repo}:.*'`,
+      "to": `image: 'docker://ghcr.io/${owner}/${repo}:\${nextRelease.version}'`,
+      "results": [{
+        "file": "action.yml",
+        "hasChanged": true,
+        "numMatches": 1,
+        "numReplacements": 1
+      }],
+      "countMatches": true
+    }]
+  });
+}
+
+const manifestExists = existsSync("./manifest.json");
+if (manifestExists && SR_DISABLE_CHROME === undefined) {
+  addPlugin("@google/semantic-release-replace-plugin", {
+    "replacements": [{
+      "files": [
+        "manifest.json"
+      ],
+      "from": `"version": ".*"`,
+      "to": `"version": "\${nextRelease.version}"`,
+      "results": [{
+        "file": "manifest.json",
+        "hasChanged": true,
+        "numMatches": 1,
+        "numReplacements": 1
+      }],
+      "countMatches": true
+    }]
+  });
+}
+
+addPlugin("@semantic-release/git", {
+  "assets": [
+    "LICENSE*",
+    "CHANGELOG.md",
+    "deno.json",
+    "deno.lock",
+    "package.json",
+    "package-lock.json",
+    "npm-shrinkwrap.json",
+    "yarn.lock",
+    "pnpm-lock.yaml",
+    "public/**/*",
+    "action.yml",
+    "manifest.json",
+    "pyproject.toml",
+    "uv.lock"
+  ],
+  "message": `chore(<%= nextRelease.type %>): release <%= nextRelease.version %> <%= nextRelease.channel !== null ? \`on \${nextRelease.channel} channel \` : '' %>[skip ci]\n\n<%= nextRelease.notes %>`
+});
+
+addPlugin("@semantic-release/github", {
+  "addReleases": "bottom",
+  "assets": [
+    {
+      "path": "pack/*.tgz",
+      "label": "Static distribution"
+    }
+  ]
+});
+
+const dockerExists = existsSync("./Dockerfile");
+if (dockerExists && SR_DISABLE_DOCKER === undefined) {
+  addPlugin("eclass-docker-fork", {
+    "baseImageName": `${owner}/${repo}`,
+    "registries": [
+      {
+        "url": "ghcr.io",
+        "imageName": `ghcr.io/${owner}/${repo}`,
+        "user": "GITHUB_REPOSITORY_OWNER",
+        "password": "GITHUB_TOKEN"
+      }
+    ]
+  });
+}
+
+addPlugin("semantic-release-major-tag");
+
+if (GITHUB_ACTIONS !== undefined) {
+  addPlugin("@semantic-release/exec", {
+    successCmd: `echo 'RELEASE_TAG=v\${nextRelease.version}' >> $GITHUB_ENV
+echo 'RELEASE_VERSION=\${nextRelease.version}' >> $GITHUB_ENV
+echo 'release-tag=v\${nextRelease.version}' >> $GITHUB_OUTPUT
+echo 'release-version=\${nextRelease.version}' >> $GITHUB_OUTPUT`,
+  });
+}
+
+module.exports = releaseConfig;
